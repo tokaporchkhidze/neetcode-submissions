7377f1ce1a202df7c65521fe6550772a395ffdc5
tokaporchkhidze <63641399+tokaporchkhidze@users.noreply.github.com>
2026-07-25 11:46:56 +0400

    Add: three-integer-sum - submission-3

---
+++ Data Structures & Algorithms/three-integer-sum/submission-3.rs
@@ -0,0 +1,31 @@
+impl Solution {
+    pub fn three_sum(mut nums: Vec<i32>) -> Vec<Vec<i32>> {
+        nums.sort_unstable();
+        let mut res = vec![];
+        for i in 0..nums.len()-2 {
+            let num = nums[i];
+
+            if i > 0 && num == nums[i - 1] {
+                continue;
+            }
+            let mut left = i + 1;
+            let mut right = nums.len() - 1;
+            while left < right {
+                if nums[left] + nums[right] + num == 0 {
+                    res.push(vec![num, nums[left], nums[right]]);
+                    left += 1;
+                    right -= 1;
+                    while left < right && nums[left] == nums[left - 1] {
+                        left += 1;
+                    }
+                } else if nums[left] + nums[right] + num < 0 {
+                    left += 1;
+                } else {
+                    right -= 1;
+                }
+            }
+        }
+        res
+    }
+
+}
