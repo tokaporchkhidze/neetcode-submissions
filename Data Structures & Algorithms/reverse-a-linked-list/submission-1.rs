497824850016f99d03932f01fd0664205107b1c3
tokaporchkhidze <63641399+tokaporchkhidze@users.noreply.github.com>
2026-08-05 16:13:55 +0400

    Add: reverse-a-linked-list - submission-1

---
+++ Data Structures & Algorithms/reverse-a-linked-list/submission-1.rs
@@ -0,0 +1,30 @@
+// Definition for singly-linked list.
+// #[derive(PartialEq, Eq, Clone, Debug)]
+// pub struct ListNode {
+//     pub val: i32,
+//     pub next: Option<Box<ListNode>>,
+// }
+//
+// impl ListNode {
+//     #[inline]
+//     pub fn new(val: i32) -> Self {
+//         ListNode { next: None, val }
+//     }
+// }
+
+impl Solution {
+    pub fn reverse_list(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        let mut prev: Option<Box<ListNode>> = None;
+        let mut curr = head;
+
+        while let Some(mut node) = curr {
+            let tmp = node.next.take();
+            node.next = prev;
+            prev = Some(node);
+            curr = tmp;
+        }
+
+        prev
+    }
+}
+
